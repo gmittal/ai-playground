db9c20e82b499c56edf4472d56e681cfec915e8a
Gautam Mittal <gautam@mittal.net>
2022-06-29 17:13:15 -0700

    init comma10k and mnist

--- mnist/configs/base.py
+++ mnist/configs/base.py
@@ -8,19 +8,13 @@ def get_config():
     config.np_seed = 42
 
     # optimizer
-    # uses full batch gradient descent
     config.learning_rate = 3e-3
     config.beta1 = 0.9
     config.beta2 = 0.999
+    config.batch_size = 128
     config.train_steps = 500
 
-    # random kernel
-    config.fourier_feat = True
-    config.kernel_dim = 256
-    config.initial_variance = 20.0
-
-    # dataset/dataloader
-    config.image_path = 'hendrix.jpg'
+    # dataloader
     config.num_workers = 0
 
     config.logging_interval = 10
